56995358bdd3dcfdc85d2e682b2c13a463f45c88
FloofyFlare <raymondflowersiv@gmail.com>
2022-11-22 19:54:05 -0500

    sign up button commit

---
+++ project-y/src/components/EmailSignUp.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>The sign up button</h1>
+    <form id="signup-form" @submit.prevent="handleSubmit">
+      <input
+        v-model="signUpEmail"
+        @keyup.enter="handleSubmit"
+        placeholder="email"
+        type="email"
+        class="email-box"
+      />
+      <button type="submit" class="submit-button">Join the Future</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const signUpEmail = ref("example@company.com");
+
+const handleSubmit = () => {
+  console.log(signUpEmail.value);
+  alert("processing");
+};
+</script>
+
+<style scoped lang="scss">
+.email-box {
+  height: 30px;
+  width: auto;
+  border-color: black;
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 30px;
+  padding: 5px 5px 5px 20px;
+}
+.submit-button {
+  height: 40px;
+  width: auto;
+  position: relative;
+  margin-left: 10px;
+  width: auto;
+  background-color: rgb(138, 159, 255);
+  color: white;
+  border-radius: 30px;
+  border-style: none;
+  padding: 10px;
+}
+</style>
--- project-y/src/views/HomeView.vue
+++ project-y/src/views/HomeView.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <EmailButton></EmailButton>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+<script setup lang="ts">
+import EmailButton from "@/components/EmailSignUp.vue"; // @ is an alias to /src
 </script>
