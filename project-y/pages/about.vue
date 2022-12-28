91423cf6a580415f10f60305944f71994e9f8d09
Suzaku17 <92160925+Suzaku17@users.noreply.github.com>
2022-12-28 18:09:40 -0500

    Add files via upload

---
+++ project-y/pages/about.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <nav>
+        <NuxtLink to="index.vue">Home</NuxtLink>
+      </nav>
+      <EmailSignUp />
+      <body>
+        <h1>
+            Raymend
+        </h1>
+        <p>
+
+        </p>
+        <h1>    
+            Darius
+        </h1>
+        <p>
+
+        </p>
+        <h1>
+            Caleb
+        </h1>
+        <p>
+
+        </p>
+      </body>
+    </div>
+  </template>
+  <script lang="ts">
+  import EmailSignUp from '~/components/EmailSignUp.vue';
+  
+  export default {
+      name: "IndexPage",
+      components: { EmailSignUp }
+  }
+  </script>
+  
+  <style scoped lang="scss">
+  @import "assets/scss/appStyles.scss";
+  </style>
+  
+  
