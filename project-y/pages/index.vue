621693d646ddef94afb13946b66ae43c267bbbab
Suzaku17 <92160925+Suzaku17@users.noreply.github.com>
2022-12-29 20:42:25 -0500

    Add files via upload
    Added more to Index and about me (not completely finished yet)

--- project-y/pages/index.vue
+++ project-y/pages/index.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>
-    <nav>
-      <NuxtLink to="about.vue">About</NuxtLink>
-    </nav>
-    <EmailSignUp />
+  <head>
+    <title> Company name here </title>
+  </head>
+    
     <body>
-       v
+      <header>
+        <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+        <a class="navbar-brand" href="#">Company Name or something</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#">Main thing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.vue">About</a>
+                </li>
+            </ul>
+        </div>
+        </nav>
+      </header>
     </body>
-  </div>
+ 
+
+  <footer>
+        <p>
+        (for future referce)
+        copyright notice
+        link to the privacy policy
+        Sitemap
+        company email/contact info:
+        (mini-logo goes here)
+       <EmailSignUp/>
+        </p>
+  </footer>
+
 </template>
 <script lang="ts">
 import EmailSignUp from '~/components/EmailSignUp.vue';
