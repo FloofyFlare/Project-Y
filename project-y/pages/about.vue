ea2bed1dd8b5a9b73725747dbbdd6faeb3ce80a4
Suzaku17 <92160925+Suzaku17@users.noreply.github.com>
2022-12-29 20:42:25 -0500

    Add files via upload
    Added more to Index and about me (not completely finished yet)

--- project-y/pages/about.vue
+++ project-y/pages/about.vue
@@ -5,26 +5,64 @@
       </nav>
       <EmailSignUp />
       <body>
+        <header>
+        <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+        <a class="navbar-brand" href="#">Company Name or something</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#">Something</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.vue">About</a>
+                </li>
+            </ul>
+        </div>
+        </nav>
+      </header>
+
         <h1>
             Raymend
         </h1>
         <p>
-
+            something something something somethingsomething somethingsomething something
+            something somethingsomething somethingsomething somethingsomething something
+            something somethingsomething somethingsomething somethingsomething something
+            something somethingsomething somethingsomething somethingsomething something
         </p>
         <h1>    
             Darius
         </h1>
         <p>
-
+            something something something somethingsomething somethingsomething something
+            something somethingsomething somethingsomething somethingsomething something
+            something somethingsomething somethingsomething somethingsomething something
+            something somethingsomething somethingsomething somethingsomething something
         </p>
         <h1>
             Caleb
         </h1>
         <p>
-
+            something something something somethingsomething somethingsomething something
+            something somethingsomething somethingsomething somethingsomething something
+            something somethingsomething somethingsomething somethingsomething something
+            something somethingsomething somethingsomething somethingsomething something
         </p>
       </body>
     </div>
+
+
+
+    <footer>
+        <p>
+
+        </p>
+    </footer>
+
   </template>
   <script lang="ts">
   import EmailSignUp from '~/components/EmailSignUp.vue';
