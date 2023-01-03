2dc5cbb1216192564fdb6b54fd3e6db957486cf0
FloofyFlare <raymondflowersiv@gmail.com>
2023-01-03 18:25:57 -0500

    adding daisy ui

--- yuuera/nuxt.config.ts
+++ yuuera/nuxt.config.ts
@@ -4,8 +4,9 @@ export default defineNuxtConfig({
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/scss/appStyles.scss'],
 
+  //I cant mess with the Nuxt tailwindwidnd:{} file Oof or daisy will not work
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -22,4 +23,4 @@ export default defineNuxtConfig({
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-})
+});
