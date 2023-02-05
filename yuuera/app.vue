216f8b40b254b3e9a8921ba7fb0b34c0dc4e5b6c
FloofyFlare <raymondflowersiv@gmail.com>
2023-02-04 21:46:51 -0500

    Seo corrected

--- yuuera/app.vue
+++ yuuera/app.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 useHead({
-  title: 'Yuuera, The Era of "Yuu',
+  title: 'Yuuera, Crypto Based E-commerce Built For Yuu',
   meta: [
     {
-    name: 'An Ecommerce Solution powered by crypto and blockchiain technology. Secure, Trustworthy, and Innovative Ecommerce at your hands.',
-  }
+    name: 'description',
+    content: 'An Ecommerce Solution powered by crypto and blockchiain technology. Secure, Trustworthy, and Innovative Ecommerce at your hands.',
+    }
   ]
 });
 </script>
