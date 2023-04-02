c2eed5ddce9ee5e4f74735f8931a1464ed32ef78
Suzaku17 <c.hunt7264@gmail.com>
2023-04-02 07:03:53 -0400

    Responsive changes (need to fix one part)

--- yuuera/pages/about.vue
+++ yuuera/pages/about.vue
@@ -1,139 +1,186 @@
 <template>
-
-    <head>
-      <title class="text-base-100">Project-Y</title>
-    </head>
-      <header class="bg-orange-600 text-base-200">
-          <div class="navbar flex-1">
-            <NuxtLink class="btn btn-ghost normal-case text-xl text-neutral" to="/">Yuuera</NuxtLink>
-          </div>
-          <hr class="h-px my-8 bg-neutral border-0 dark:bg-grey-200">
-      </header>
-    <body class="bg-accent">
-      
-      <div class="space-x-96 mb-64">
-
-        <div class="avatar">
-          <div class=" w-48 rounded">
-            <img src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
-          </div>
-        </div>
-
-        <div class="avatar">
-          <div class=" w-48 rounded">
-            <img src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
+  
+  <head>
+    <title class="text-base-100">Project-Y</title>
+  </head>
+  <header class="bg-primary text-base-200 ">
+    <div class="navbar flex-1">
+      <NuxtLink class="btn btn-ghost normal-case text-xl text-neutral" to="/">Yuuera</NuxtLink>
+    </div>
+  </header>
+  <body class="bg-accent">
+    
+    <div>
+      <div class=" hero aboutCityImage shadow-2xl">
+        <div class="w-full h-full flex justify-center items-center backdrop-blur-sm hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+          <div class="max-w-4xl sm:text-left">
+            <h1 class=" text-primary text-6xl md:text-9xl font-semibold  ">Putting <span class="text-info">Yuu</span> back in the equation</h1>
+            
           </div>
         </div>
-        </div>
-
-
-      <div class="bg-accent">
-        <div class="mt-16 mb-16">
-            <h3 class="text-primary font-bold text-2xl">Our Vision</h3>
-              <div class="mb-16 flex justify-center">
-                <div class=" bg-info h-40 w-2/3 rounded-3xl shadow-lg">
-                  <p class="text-neutral">
-                    Something something programming technology blockchain pogchamp lizard men overwatch 2 sucks dksflasdjfklt6r7sghjdsfastghjudfgashjIDfguyihzsXfcgouisdgvfiuysdviyufesdr
-                  </p>
-                </div>
-              </div>
-        </div>
-        
-        <div class="mt-64 mb-16">
-            <h3 class="text-primary font-bold text-2xl">Meet the Team</h3>
-        </div>
-
-        <div class=" space-x-24">
-          <div class="avatar">
-            <div class="w-48 rounded">
-              <img src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
-            </div>
-          </div>
-          
-          <div class="avatar">
-            <div class="flex justify-center w-48 rounded">
-              <img src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
-            </div>
-          </div>
-
-          <div class="avatar">
-            <div class="flex justify-end w-48 rounded">
-              <img src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
-            </div>
-          </div>
-        </div>
-        
-        <div class="mt-64 mb-16">
-            <h3 class="text-primary font-bold text-2xl">Values</h3>
-        </div>
-        <div class="mt-16 mb-16 flex justify-center">
-          <div class="bg-info w-2/3 rounded-3xl h-40 shadow-lg">
-            <h3 class="text-neutral font-bold text-xl p-2"> Privacy policy</h3>
-            <p class="text-neutral">
-                
-            </p>
-          </div>
-        </div>
-
-        <div class="mb-16 flex justify-center">
-          <div class=" bg-info h-40 w-2/3 rounded-3xl shadow-lg">
-            <h3 class="text-neutral font-bold text-xl p-2">Consumer thing</h3>  
-            <p class="text-neutral">
-                
-            </p>
-          </div>
-        </div>
-
-        <div class="mb-16 flex justify-center">
-
-          <div class=" bg-info h-40 w-2/3 rounded-3xl shadow-lg">
-            <h3 class="text-neutral font-bold text-xl p-2">Community</h3>  
-            <p class="text-neutral">
-                
-            </p>
-          </div>
-        </div>
-
-        </div>
-        <div>
-          <EmailSignUp/>
-        </div>
-      
-      </body>
-    <div class="bg-accent">
-      <div class="bg-orange-600">
-        <footer class="footer footer-center p-10  text-neutral rounded">
-          <div class="grid grid-flow-col gap-4">
-            <p class="font-bold text-2xl">Contact Us</p>
-          </div> 
-        </footer>
-
-        <footer class="footer px-10 py-4  bg-natural text-base-content ">
-          <div class="items-center grid-flow-col text-neutral">
-              <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
-              <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-              <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-          </div> 
-          <div class="md:place-self-center md:justify-self-end">
-            <div class="grid grid-flow-col gap-4 text-neutral">
-              <p>Companyemail.com (plus logo)</p>
-              <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-            </div>
-          </div>
-        </footer>
       </div>
     </div>
-    </template>
-    <script lang="ts">
-    import EmailSignUp from '~/components/EmailSignUp.vue'
+
+
+    <section class="bg-accents pt-24">
+      <div class="mt-16 mb-16">
+          <h3 class="text-primary font-bold text-4xl md:text-7xl">The Internet Powered by <span class="text-info">Yuu</span></h3>
+            <div class="md:flex">
+            <div class="mb-16 flex justify-center">
+              <div class="mt-4 h-adjustment w-2/3 rounded-3xl">
+                <p class="pt-24 text-neutral leading-loose text-xl md:text-3xl text-block">
+                  Hello, welcome to the Yuuera team we are excited to have you here. 
+                  Our goal is to utilize the power of blockchain to
+                  create a better future for the internet together. We value the decentralization
+                  of the internet as key to creating a more vibrant, efficient, and Community
+                  based tech scene. Our mission is to give control of the internet back to the 
+                  people in shopping and beyond. The internet was not made by one person. Let's keep it that way. 
+                  We would be honored to have you join us in our journey to change the future of the internet brining 
+                  the power to the people. 
+                </p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
+      
+    <section class="bg-secondary mt-16 flex">
+      <div class="flex ">
+
+      
+      <div class="triangle-divider">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z" class="shape-fill"></path>
+          </svg>
+      </div>
+      
+      <div class="h-screen pl-24">
+        <div class="flex flex-row justify-center space-x-24 mt-44  ...">
+          <div>
+            <div class="flex rounded-3xl bg-accent p-4  team-member flex-col ...">
+              <div class="flex w-48 rounded">
+                <img src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
+              </div>
+              <div>
+                <p class="font-medium p-8 text-2xl">Darrius</p>
+                <p class="font-medium text-lg">Chief Marketing Officer</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="flex rounded-3xl bg-accent p-4  team-member flex-col ...">
+              <div class="flex w-48 rounded">
+                <img src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
+              </div>
+              <p class="font-medium p-8 text-2xl">Raymond </p>
+                <p class="font-medium text-lg">Cheif Executive Officer</p>
+            </div>
+          </div>
+          <div class="">
+            <div class="flex rounded-3xl bg-accent p-4 team-member flex-col ...">
+              <div class="flex w-48 rounded">
+                <img src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
+              </div>
+              <p class="font-medium p-8 text-2xl">Caleb </p>
+                <p class="font-medium text-lg">Cheif Technology Officer</p>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+      <div class="mb-16 md:flex justify-center">
+        <div class="mt-4 h-adjustment w-2/3 rounded-3xl">
+          <h1 class="text-accent font-bold text-6xl pt-24 text-right sm:text-left">Meet the Team</h1>
+          <h2 class="pt-24 text-neutral leading-loose text-l md:text-2xl  text-block text-accent font-semibold">
+            Hello, welcome to the Yuuera team we are excited to have you here. 
+            Our goal is to utilize the power of blockchain and cryptocurrency to
+            create a better future for the internet together. We value the decentralization
+            of the internet as key to creating a more vibrant, efficient, and Community
+            based tech scene. Our mission is to give control of the internet back to the 
+            people in shopping and beyond. The internet was not made by one person. Let's keep it that way. 
+            We would be honored to have you join us in our journey to change the future of the internet brining 
+            the power to the people. 
+          </h2>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      
+      <div class="mt-24 mb-16">
+          <h3 class="text-primary font-bold text-4xl md:text-6xl">Built for <span class="text-info">Yuu</span></h3>
+      </div>
+      
+      <div class="mt-10 mb-10 flex justify-center">
+        <div class="flex flex-row bg-info w-2/3 rounded-3xl h-48 shadow-lg">
+          <img class="h-46 w-64 rounded-3xl md:w-80 lg:w-96" src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
+          <h3 class="text-neutral font-bold text-l lg:text-xl p-2">Consumer Focused: </h3>
+          <p class="text-neutral leading-loose text-l lg:text-2xl">
+              Providing great products and services 
+              is one thing. Creating products that you value is success.
+          </p>
+        </div>
+      </div>
+
+      <div class="mb-10 flex justify-center">
+        <div class="flex flex-row bg-info w-2/3 rounded-3xl h-48 shadow-lg">
+          <h3 class="text-neutral font-bold text-l lg:text-xl p-2">Privacy: </h3>  
+          <p class="text-neutral leading-loose text-l lg:text-2xl">
+              Secure peer to peer transacitons on without
+              reliance on third part organizations or entities. EVEN US!
+          </p>
+          <img class="h-48 w-64 rounded-3xl md:w-80 lg:w-96" src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
+        </div>
+      </div>
+
+      <div class="mb-16 flex justify-center">
+
+        <div class=" flex flex-row bg-info w-2/3 rounded-3xl h-48 shadow-lg">
+          <img class="h-48 w-64 rounded-3xl md:w-80 lg:w-96" src="https://assets.eflorist.com/site/EF-12844/Homepage/Thank%20You%20to%20cust_5.png" />
+          <h3 class="text-neutral font-bold text-l lg:text-xl p-2">Community: </h3>  
+          <p class="text-neutral leading-loose text-sm lg:text-2xl">
+              Blockchain is internet freedom for us, providing
+              the space for tomorrows creativity, innovation, and experiences.
+          </p>
+        </div>
+      </div>
+    </section>
     
-    export default {
-      name: 'IndexPage',
-      components: { EmailSignUp },
-    }
-    </script>
-    
-    <style scoped lang="scss">
-    @import 'assets/scss/appStyles.scss';
-    </style>
-    
+      <div class="mt-60 Spt-24 h-adjustment">
+        <ClientOnly>
+          <EmailSignUp></EmailSignUp>
+        </ClientOnly>
+      </div>
+  </body>
+
+  <footer class="bg-accent">
+    <div class="bg-orange-600">
+      <div class="footer footer-center p-10  text-neutral rounded">
+        <div class="grid grid-flow-col gap-4">
+          <p class="font-bold text-2xl">Contact Us</p>
+        </div> 
+      </div>
+
+      <div class="footer px-10 py-4  bg-natural text-base-content ">
+        <div class="items-center grid-flow-col text-neutral">
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+        </div> 
+        <div class="md:place-self-center md:justify-self-end">
+          <div class="grid grid-flow-col gap-4 text-neutral">
+            <p>Companyemail.com (plus logo)</p>
+            <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  </template>
+  <script s>
+  </script>
+  
+  <style scoped lang="scss">
+  @import 'assets/scss/appStyles.scss';
+  </style>
