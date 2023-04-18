7cd2c51f3f7762ab8e07a5fae797cbd393471410
FloofyFlare <raymondflowersiv@gmail.com>
2023-04-17 22:00:27 -0400

    nginx added

--- yuuera/components/EmailSignUp.vue
+++ yuuera/components/EmailSignUp.vue
@@ -33,7 +33,7 @@ async function handleSubmit() {
   alert('processing')
   var data = { email: signUpEmail.value }
   //change to yuuera for production
-  await $fetch('http://127.0.0.1:8000/newsletter/', {
+  await $fetch('http://0.0.0.0:8000/newsletter/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
