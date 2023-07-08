d2932d0e62298bb5bc17ad613ac8747826792b35
Raymond Flowers <flogamesapps@gmail.com>
2023-07-08 10:10:46 -0400

    final again...

--- yuuera/pages/about.vue
+++ yuuera/pages/about.vue
@@ -44,6 +44,7 @@
       </div>
     </section>
     
+   
     <section class="flex justify-center">
       <div class="flex justify-center items-center md:w-2/3 md:mt-40 md:mb-12">
         <div class="rounded-3xl bg-secondary z-40">
@@ -75,8 +76,9 @@
           <div class="flex  justify-center items-center">
             <div class="hero-content text-center text-neutral-content">
               <div class="max-w-4xl sm:text-left">
-                <h1 class="text-primary font-bold text-4xl md:text-7xl">
+                <h1 class="flex text-primary font-bold text-4xl md:text-7xl">
                   Who We Are
+                  
                 </h1>
               </div>
             </div>
@@ -84,14 +86,55 @@
           <div class="mb-24 flex justify-center">
             <div class="md:p-12 p-4  rounded-3xl">
               <p
-                class="text-xl text-neutral text-center leading-loose tracking-widest text-l lg:text-2xl"
+                class="text-xl pb-4 text-neutral text-center leading-loose tracking-widest text-l lg:text-2xl"
               >
-                We transform ideas into cutting-edge software. Since the 2019 crypto
-                craze, we've been pioneers in the crypto space, turning it from a
-                fad into a reality. Through ideation, problem-solving, and legal
-                work, we bring the dream of crypto to life. Join us as we shape the
-                future of technology, one innovative line of code at a time.
+                We transform ideas into cutting-edge software. Through ideation, problem-solving, and hard work, we bring the dream of crypto to life. Join us as we shape the
+                future of technology, one line of code at a time.
               </p>
+              <div class="flex w-full">
+                <div class="flex-col w-1/2 p-2">
+                  <nuxt-img class="grayscale rounded-full" alt="Picture of Caleb Hunt" src="/images/Caleb.jpg"/>
+                  <div class="flex justify-center ">
+                    <h1 class="md:text-lg text-neutral pr-4">Caleb Hunt: CTO</h1>
+                      <a href="https://twitter.com/"
+                        ><svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          class="fill-current"
+                        >
+                        <path
+                          d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+                        ></path></svg>
+                      </a>
+                  </div>
+                </div>
+                <div class="flex-col w-1/2 p-2">
+                  <nuxt-img class="grayscale rounded-full" src="/images/Raymond.jpg"/>
+                  <div class="flex justify-center ">
+                    <h1 class="md:text-lg text-neutral pr-4">Raymond Flowers: CEO</h1>
+                      <a href="https://twitter.com/rev_cardano"
+                        ><svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          class="fill-current"
+                        >
+                        <path
+                          d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+                        ></path></svg>
+                      </a>
+                      <a href="https://www.linkedin.com/in/raymond-flowers-4b7aa521b/">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="fill-current" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+                      </a>
+                  </div>
+                </div>
+                <p></p>
+              </div>
+             
+              
             </div>
           </div>
         </div>
