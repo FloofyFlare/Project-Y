6ddd13a5f1a4a9b23b6c5feaab5d2591ce06eaf8
Raymond Flowers <flogamesapps@gmail.com>
2023-08-27 19:31:27 -0400

    added a wallet enabler for eternl

---
+++ yuuera/components/LogIn.vue
@@ -0,0 +1,18 @@
+<script lang="js">
+ const isWalletCheck = async () => {
+    try {
+      const isCardano = window && window.cardano
+      const isEternlInstalled = isCardano && window.cardano.eternl
+      const isEternlEnabled = isEternlInstalled ? await window.cardano.eternl.isEnabled() : false
+      cardano.eternl.enable()
+
+      console.log()
+    }
+    catch {
+      console.log('error')
+    }
+  }
+
+  export default {}
+
+</script>
